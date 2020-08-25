831927cd7e8466bf318f4d13f0cbb0e06ead2b89
laura <laurensia.esmeralda@.ti.ukdw.ac.id>
2020-08-25 13:06:33 +0700

    updateeeeee
    neh kurang edit; update bagian tampilan detail

--- resources/views/detailUmat.blade.php
+++ resources/views/detailUmat.blade.php
@@ -13,40 +13,108 @@
                                  @foreach( $umat_nama as $dup )
                                          <div class="row">
                                             <div class="col-4 w-25">Nama Lengkap </div>
-                                            <li class="list-group col-4">:{{ $dup->umat_nama}}</li>
+                                            <li class="list-group col-4">: {{ $dup->umat_nama}}</li>
                                          </div>
                                          <div class="row">
                                             <div class="col-4 w-25">NIK </div>
-                                            <li class="list-group">:{{ $dup->umat_ktp}}</li>
+                                            <li class="list-group">: {{ $dup->umat_ktp}}</li>
                                          </div>
-                                         
-                                         <li class="list-group">Nomor KK: {{ $dup->umat_kk}}</li>
-                                         <li class="list-group">Tempat Tanggal Lahir: {{ $dup->umat_tempat_lahir}}, {{ $dup->umat_tanggal_lahir}}</li>
-                                         <li class="list-group">Status Nikah: {{ $dup->status_nikah_nama}}</li>
-                                         <li class="list-group">Hubungan Keluarga {{ $dup->hubungan_keluarga_nama}}</li>
-                                         <li class="list-group">Status Rumah: {{ $dup->status_rumah_nama}}</li>
-                                         <li class="list-group">Pendidikan: {{ $dup->pendidikan_nama}}</li>
-                                         <li class="list-group">Golongan Darah: {{ $dup->golongan_darah_nama}}</li>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Nomor KK </div>
+                                            <li class="list-group">: {{ $dup->umat_kk}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Tempat Tanggal Lahir </div>
+                                            <li class="list-group">: {{ $dup->umat_tempat_lahir}}, {{ $dup->umat_tanggal_lahir}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Status Nikah </div>
+                                            <li class="list-group">: {{ $dup->status_nikah_nama}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Hubungan Keluarga </div>
+                                            <li class="list-group">: {{ $dup->hubungan_keluarga_nama}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Status Rumah</div>
+                                            <li class="list-group">: {{ $dup->status_rumah_nama}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Pendidikan</div>
+                                            <li class="list-group">: {{ $dup->pendidikan_nama}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Golongan Darah </div>
+                                            <li class="list-group">: {{ $dup->golongan_darah_nama}}</li>
+                                         </div> 
                                          @if($dup->umat_jenis_kelamin === 1)
-                                            <li class="list-group">Jenis Kelamin: Perempuan</li>
+                                            <div class="row">
+                                                <div class="col-4 w-25">Jenis Kelamin </div>
+                                                <li class="list-group">: Perempuan</li>
+                                            </div>
                                          @else
-                                            <li class="list-group">Jenis Kelamin: Laki-Laki</li>
+                                            <div class="row">
+                                                <div class="col-4 w-25">Jenis Kelamin </div>
+                                                <li class="list-group">: Laki-laki</li>
+                                            </div>
                                          @endif
-                                         <li class="list-group">Alamat: {{ $dup->umat_alamat}}</li>
-                                         <li class="list-group">Kabupaten/Kota: {{ $dup->umat_kota_kab}}</li>
-                                         <li class="list-group">Kecamatan: {{ $dup->umat_kecamatan}}</li>
-                                         <li class="list-group">Kelurahan: {{ $dup->umat_kelurahan}}</li>
-                                         <li class="list-group">Nomor Telepon: {{ $dup->umat_handphone}}</li>
-                                         <li class="list-group">Email: {{ $dup->umat_email}}</li>
-                                         <li class="list-group">Jurusan: {{ $dup->umat_jurusan}}</li>
-                                         <li class="list-group">Profesi: {{ $dup->profesi_nama}}</li>
-                                         <li class="list-group">Pekerjaan: {{ $dup->pekerjaan_nama}}</li>
-                                         <li class="list-group">Keterampilan: {{ $dup->umat_keterampilan}}</li>
-                                         <li class="list-group">Status Aktivitas Sosial: {{ $dup->status_aktivitas_sosial_nama}}</li>
-                                         <li class="list-group">Disabilitas: {{ $dup->tuna_nama}}</li>
-                                         <li class="list-group">Penghasilan: {{ $dup->penghasilan}}</li>
-                                         <li class="list-group">Narasi: {{ $dup->narasi}}</li>       
-                                @endforeach
+                                         <div class="row">
+                                            <div class="col-4 w-25">Alamat</div>
+                                            <li class="list-group">: {{ $dup->umat_alamat}}</li>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-4 w-25">Kabupaten/Kota</div>
+                                            <li class="list-group">: {{ $dup->umat_kota_kab}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Kecamatan</div>
+                                            <li class="list-group">: {{ $dup->umat_kecamatan}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Kelurahan</div>
+                                            <li class="list-group">: {{ $dup->umat_kelurahan}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Nomor Telepon</div>
+                                            <li class="list-group">: {{ $dup->umat_handphone}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Email</div>
+                                            <li class="list-group">: {{ $dup->umat_email}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Jurusan</div>
+                                            <li class="list-group">: {{ $dup->umat_jurusan}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Profesi</div>
+                                            <li class="list-group">: {{ $dup->profesi_nama}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Pekerjaan</div>
+                                            <li class="list-group">: {{ $dup->pekerjaan_nama}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Keterampilan</div>
+                                            <li class="list-group">: {{ $dup->umat_keterampilan}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Status Aktivitas Sosial</div>
+                                            <li class="list-group">: {{ $dup->status_aktivitas_sosial_nama}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Disabilitas</div>
+                                            <li class="list-group">: {{ $dup->tuna_nama}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Penghasilan</div>
+                                            <li class="list-group">: {{ $dup->penghasilan}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Narasi</div>
+                                            <li class="list-group">: {{ $dup->narasi}}</li>       
+                                        </div>
+                                         @endforeach
                             </div>
                         </div>
                     </div>
@@ -55,23 +123,65 @@
                             <div class="card-body">
                                 <h5 class="card-title">Informasi Gerejawi</h5>
                                  @foreach( $umat_nama as $dup )
-                                         <li class="list-group">Nama Baptis: {{ $dup->umat_nama_baptis}}</li>
-                                         <li class="list-group">Nomor Buku Baptis: {{ $dup->umat_buku_baptis}}</li>
-                                         <li class="list-group">Nomor Baptis: {{ $dup->umat_nomer_baptis}}</li>
-                                         <li class="list-group">Keuskupan Baptis: {{ $dup->keuskupan_nama}}</li>
-                                         <li class="list-group">Paroki Baptis: {{ $dup->paroki_nama}}</li>
-                                         <li class="list-group">Suku: {{ $dup->suku_nama}}</li>
-                                         <li class="list-group">Status Ekonomi: {{ $dup->status_ekonomi_nama}}</li>
-                                         <li class="list-group">Agama: {{ $dup->agama_nama}}</li>
-                                         <li class="list-group">Kevikepan: {{ $dup->kevikepan_nama}}</li>
-                                         <li class="list-group">Paroki: {{ $dup->paroki_nama}}</li>
-                                         <li class="list-group">Wilayah: {{ $dup->wilayah_nama}}</li>
-                                         <li class="list-group">Lingkungan: {{ $dup->lingkungan_nama}}</li>
+                                        <div class="row">
+                                            <div class="col-4 w-25">Nama Baptis</div>
+                                            <li class="list-group">: {{ $dup->umat_nama_baptis}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Nomor Buku Baptis</div>
+                                            <li class="list-group">: {{ $dup->umat_buku_baptis}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Nomor Baptis</div>
+                                            <li class="list-group">: {{ $dup->umat_nomer_baptis}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Keuskupan Baptis</div>
+                                            <li class="list-group">: {{ $dup->keuskupan_nama}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Paroki Baptis</div>
+                                            <li class="list-group">: {{ $dup->paroki_nama}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Suku</div>
+                                            <li class="list-group">: {{ $dup->suku_nama}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Status Ekonomi</div>
+                                            <li class="list-group">: {{ $dup->status_ekonomi_nama}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Agama</div>
+                                            <li class="list-group">: {{ $dup->agama_nama}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Kevikepan</div>
+                                            <li class="list-group">: {{ $dup->kevikepan_nama}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Paroki</div>
+                                            <li class="list-group">: {{ $dup->paroki_nama}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Wilayah</div>
+                                            <li class="list-group">: {{ $dup->wilayah_nama}}</li>
+                                         </div>
+                                         <div class="row">
+                                            <div class="col-4 w-25">Lingkungan</div>
+                                            <li class="list-group">: {{ $dup->lingkungan_nama}}</li>
+                                         </div>
                                          @if($dup->umat_meninggal === 1)
-                                            <li class="list-group">Status Meninggal: Meninggal</li>
-                                         @else
-                                            <li class="list-group">Status Meninggal: Belum Meninggal</li>
-                                         @endif
+                                            <div class="row">
+                                                <div class="col-4 w-25">Status Meninggal</div>
+                                                <li class="list-group">: Meninggal</li>
+                                            </div>
+                                        @else
+                                            <div class="row">
+                                                <div class="col-4 w-25">Status Meninggal</div>
+                                                <li class="list-group">: Meninggal</li>
+                                            </div>
+                                        @endif
                                 @endforeach
                             </div>
                         </div>
@@ -86,7 +196,10 @@
                     <div class="card border-danger">
                         <div class="card-body">
                             <h5 class="card-title">Berkas Bukti</h5>
-                            <li class="list-group">Tanggal Update: {{ $dup->tgl_update}}</li>
+                                <div class="row">
+                                    <div class="col-4 w-25">Tanggal Update</div>
+                                    <li class="list-group">: {{ $dup->tgl_update}}</li>
+                                </div>
                             <!-- <a href=""  name="Upload" id=""><button type="button" class="btn btn-danger">Upload</button></a>
                             <a href=""  name="Upload" id=""><button type="button" class="btn btn-danger">Upload</button></a>
                             <a href=""  name="Upload" id=""><button  type="button" class="btn btn-danger">Upload</button></a> -->
